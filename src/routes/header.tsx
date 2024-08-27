32335de3330849a2975553ef20a925344a35e35f
angel-luis <angel.luis@me.com>
2024-08-27 16:55:03 +0200

    Style adjust

--- src/routes/header.tsx
+++ src/routes/header.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function HeaderLayout() {
   return (
     <>
-      <nav className="flex justify-between mx-8">
+      <nav className="flex justify-between mx-8 my-2">
         <h1>Logo</h1>
         <ul>
           <li>Home</li>

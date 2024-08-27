1d8b37a96a9fb190c0caa9a925fae6020f345660
angel-luis <angel.luis@me.com>
2024-08-27 20:36:07 +0200

    Implement router links

--- src/routes/header.tsx
+++ src/routes/header.tsx
@@ -1,15 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function HeaderLayout() {
+  const menuLinks = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Shop",
+      path: "/shop",
+    },
+    {
+      name: "Contact",
+      path: "/contact",
+    },
+    {
+      name: "Sign In",
+      path: "/signin",
+    },
+  ];
+
   return (
     <>
       <nav className="flex justify-between mx-8 my-2">
         <h1>Logo</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+        <div className="flex gap-4">
+          {menuLinks.map((link) => (
+            <Link key={link.path} to={link.path} className="hover:underline">
+              {link.name}
+            </Link>
+          ))}
+        </div>
       </nav>
 
       <Outlet />

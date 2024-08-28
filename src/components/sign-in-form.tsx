b704395dc43966fd63bafda05f0eacf2b1972708
angel-luis <angel.luis@me.com>
2024-08-28 20:28:08 +0200

    Implement sign in login form handling

--- src/components/sign-in-form.tsx
+++ src/components/sign-in-form.tsx
@@ -1,16 +1,33 @@
 import { useFormik } from "formik";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Button from "@/components/button";
 import Input from "@/components/input";
+import { handleEmailAuth } from "@/data/firebase";
 
 export default function SignInForm() {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
   const formik = useFormik({
     initialValues: {
       emailSignIn: "",
       passwordSignIn: "",
     },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: async (values) => {
+      const result = await handleEmailAuth(
+        "signIn",
+        values.emailSignIn,
+        values.passwordSignIn
+      );
+
+      if (!result?.sucess) {
+        setError("Something went wrong. Please try again.");
+        return;
+      }
+
+      navigate("/");
     },
   });
 
@@ -39,6 +56,15 @@ export default function SignInForm() {
       <div className="mt-4">
         <Button title="Sign In" type="submit" style="primary" />
       </div>
+
+      {error && (
+        <div
+          className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 text-center mt-2"
+          role="alert"
+        >
+          <span className="font-medium">Error:</span> {error}
+        </div>
+      )}
     </form>
   );
 }

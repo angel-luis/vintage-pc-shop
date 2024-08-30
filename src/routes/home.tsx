7504b3dbb2903ad4bbd36747a025039621acf8db
angel-luis <angel.luis@me.com>
2024-08-30 10:26:41 +0200

    Add header

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,6 +5,9 @@ export default function HomePage() {
   return (
     <>
       <HomeCTA />
+      <h2 className="text-3xl font-bold mb-8 text-center">
+        Featured Computers
+      </h2>
       <HomeDeals />
     </>
   );

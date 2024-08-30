691be3ae8e31ee6e0463c2532efd4705534c81be
angel-luis <angel.luis@me.com>
2024-08-30 09:57:17 +0200

    Implement CTA and Deals in home

---
+++ src/components/home-deals.tsx
@@ -0,0 +1,64 @@
+export default function HomeDeals() {
+  return (
+    <div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
+        <div className="h-56 w-full">
+          <a href="#">
+            <img
+              className="mx-auto h-full"
+              src="https://placehold.co/280x240"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="pt-6">
+          <a
+            href="#"
+            className="text-lg font-semibold leading-tight text-gray-900 hover:underline"
+          >
+            Apple II
+          </a>
+
+          <div className="mt-2 flex items-center gap-2">
+            <div className="flex items-center">
+              <img className="h-4 w-4" src="star.svg" />
+            </div>
+
+            <p className="text-sm font-medium text-gray-900">5.0</p>
+            <p className="text-sm font-medium text-gray-500">(2)</p>
+          </div>
+
+          <div className="mt-4 flex items-center justify-between gap-4">
+            <p className="text-2xl font-extrabold leading-tight text-gray-900">
+              $499
+            </p>
+
+            <button
+              type="button"
+              className="inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            >
+              <svg
+                className="-ms-2 me-2 h-5 w-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
+                />
+              </svg>
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,27 +1,11 @@
-import { Link } from "react-router-dom";
-
-import Button from "@/components/button";
+import HomeCTA from "@/components/home-cta";
+import HomeDeals from "@/components/home-deals";
 
 export default function HomePage() {
   return (
-    <section className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-      <img
-        className="w-full"
-        src="https://placehold.co/800x450"
-        alt="dashboard image"
-      />
-      <div className="mt-4 md:mt-0">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold">
-          Discover Vintage Computing Gems
-        </h2>
-        <p className="mb-6 font-light text-gray-500 md:text-lg">
-          Discover our collection of vintage computers, from classic desktops to
-          rare laptops. Relive the glory days of computing with our selection.
-        </p>
-        <Link to="/shop">
-          <Button title="Shop now" type="submit" style="primary" />
-        </Link>
-      </div>
-    </section>
+    <>
+      <HomeCTA />
+      <HomeDeals />
+    </>
   );
 }

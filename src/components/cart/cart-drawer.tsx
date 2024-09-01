3362402daebd7ccddc73face605c261a64bb47fd
angel-luis <angel.luis@me.com>
2024-09-01 19:32:41 +0200

    Style product cards and fix drawer overlay

--- src/components/cart/cart-drawer.tsx
+++ src/components/cart/cart-drawer.tsx
@@ -28,7 +28,7 @@ export default function CartDrawer({
   return (
     <>
       <div
-        className={`fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-300 ${
+        className={`fixed z-40 inset-0 bg-black bg-opacity-50 transition-opacity duration-300 ${
           isOpen ? "opacity-100 visible" : "opacity-0 invisible"
         }`}
         onClick={() => setOpen(false)}
--- src/components/product/product-card.tsx
+++ src/components/product/product-card.tsx
@@ -10,39 +10,41 @@ export default function ProductCard({ product }: { product: Product }) {
   const { handleAddToCart } = useContext(CartContext);
 
   return (
-    <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
-      <div className="h-56 w-full">
-        <Link to={product.path}>
-          <img className="mx-auto h-full" src={product.image} alt="" />
-        </Link>
-      </div>
-      <div className="pt-6">
-        <Link
-          to={product.path}
-          className="text-lg font-semibold leading-tight text-gray-900 hover:underline"
-        >
-          {product.title}
-        </Link>
-
-        <div className="mt-2 flex items-center gap-2">
-          <div className="flex items-center">
-            {[...Array(product.stars)].map((_, index) => (
-              <img key={index} className="h-4 w-4" src="icons/star.svg" />
-            ))}
-          </div>
-          <p className="text-sm font-medium text-gray-900">
-            {product.score.toFixed(1)}
-          </p>
-
-          <p className="text-sm font-medium text-gray-500">
-            ({product.reviews})
-          </p>
+    <div className="relative rounded-lg border bg-gray-200 p-6">
+      <div className="w95-border">
+        <div className="h-56 w-full">
+          <Link to={product.path}>
+            <img className="mx-auto h-full" src={product.image} alt="" />
+          </Link>
         </div>
+        <div className="pt-6">
+          <Link
+            to={product.path}
+            className="text-lg font-semibold leading-tight text-gray-900 hover:underline"
+          >
+            {product.title}
+          </Link>
 
-        <div className="mt-4 flex items-center justify-between gap-4">
-          <p className="text-2xl font-extrabold leading-tight text-gray-900">
-            {currencyConverter(product.price)}
-          </p>
+          <div className="flex items-center justify-between mb-4">
+            <div className="mt-2 flex items-center gap-2">
+              <div className="flex items-center">
+                {[...Array(product.stars)].map((_, index) => (
+                  <img key={index} className="h-4 w-4" src="icons/star.svg" />
+                ))}
+              </div>
+              <p className="text-sm font-medium text-gray-900">
+                {product.score.toFixed(1)}
+              </p>
+
+              <p className="text-sm font-medium text-gray-500">
+                ({product.reviews})
+              </p>
+            </div>
+
+            <p className="text-2xl font-extrabold leading-tight text-gray-900">
+              {currencyConverter(product.price)}
+            </p>
+          </div>
 
           <Button style="tertiary" onClick={() => handleAddToCart(product)}>
             <img

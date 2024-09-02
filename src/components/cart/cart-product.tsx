5f8c3037ce4059bdd0b0311ae01e8153188a2286
angel-luis <angel.luis@me.com>
2024-09-02 11:48:41 +0200

    Recycle bin icon

--- src/components/cart/cart-product.tsx
+++ src/components/cart/cart-product.tsx
@@ -109,23 +109,7 @@ export default function CartProduct({
             aria-label="Remove item"
             onClick={() => handleRemoveProduct(product)}
           >
-            <svg
-              className="h-4 w-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="red"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
-              />
-            </svg>
+            <img className="h-5" src="/icons/w98-recycle-bin.png" />
           </Button>
         </div>
       </div>

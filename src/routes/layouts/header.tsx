65004dbb359a31c23b0191a33498615dcdaf64e9
angel-luis <angel.luis@me.com>
2024-09-02 10:33:59 +0200

    Header mobile menu

--- src/routes/layouts/header.tsx
+++ src/routes/layouts/header.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 import CartButton from "@/components/cart/cart-button";
@@ -8,6 +8,7 @@ import { CartContext } from "@/contexts/cart-context";
 
 export default function HeaderLayout() {
   const { isDrawerOpen, setDrawerOpen } = useContext(CartContext);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const menuLinks = [
     {
@@ -32,17 +33,39 @@ export default function HeaderLayout() {
       <div className="flex justify-between items-center mx-8 my-2">
         <h1>Logo</h1>
         <nav className="flex gap-4 items-center">
-          {menuLinks.map((link) => (
-            <Link key={link.path} to={link.path}>
-              <Button style="secondary">
-                <img className="w-6" src={`/icons/${link.icon}`} /> {link.name}
-              </Button>
-            </Link>
-          ))}
+          <div className="hidden sm:flex gap-4 items-center">
+            {menuLinks.map((link) => (
+              <Link key={link.path} to={link.path}>
+                <Button style="secondary">
+                  <img className="w-6" src={`/icons/${link.icon}`} />{" "}
+                  {link.name}
+                </Button>
+              </Link>
+            ))}
+          </div>
+          <div className="sm:hidden">
+            <Button
+              style="primary"
+              onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+            >
+              <img className="h-8" src="icons/menu.png" />
+            </Button>
+          </div>
           <CartButton handleClick={() => setDrawerOpen(!isDrawerOpen)} />
           <CartDrawer isOpen={isDrawerOpen} setOpen={setDrawerOpen} />
         </nav>
       </div>
+      {isMobileMenuOpen && (
+        <div className="flex flex-col gap-4 bg-gray-200 p-8">
+          {menuLinks.map((link) => (
+            <Link key={link.path} to={link.path}>
+              <Button style="secondary" widthFull>
+                <img className="w-6" src={`/icons/${link.icon}`} /> {link.name}
+              </Button>
+            </Link>
+          ))}
+        </div>
+      )}
     </header>
   );
 }

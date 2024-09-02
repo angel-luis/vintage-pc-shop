d9f92e2721a159ac743b63036cb1cbb39153e70a
angel-luis <angel.luis@me.com>
2024-09-02 14:43:55 +0200

    Scroll to top when linking to other page

--- src/components/shop/shop-products-list.tsx
+++ src/components/shop/shop-products-list.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import ProductCard from "@/components/product/product-card";
 import ShopPagination from "@/components/shop/shop-pagination";
@@ -18,10 +18,6 @@ export default function ShopProductsList({
     startIndex,
     startIndex + productsPerPage
   );
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [currentPage]);
 
   return (
     <div>
--- src/routes/layouts/main.tsx
+++ src/routes/layouts/main.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 
+import ScrollToTop from "@/components/common/scrollToTop";
 import FooterLayout from "@/routes/layouts/footer";
 import HeaderLayout from "@/routes/layouts/header";
 
 export default function MainLayout() {
   return (
-    <div className="flex flex-col min-h-screen justify-stretch">
-      <HeaderLayout />
-      <div className="flex-grow px-2 md:px-4 py-4">
-        <Outlet />
+    <ScrollToTop>
+      <div className="flex flex-col min-h-screen justify-stretch">
+        <HeaderLayout />
+        <div className="flex-grow px-2 md:px-4 py-4">
+          <Outlet />
+        </div>
+        <FooterLayout />
       </div>
-      <FooterLayout />
-    </div>
+    </ScrollToTop>
   );
 }

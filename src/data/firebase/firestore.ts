6a09a50d4745bcaa642457fd6e2b4b1efa0136f9
angel-luis <angel.luis@me.com>
2024-09-03 13:24:05 +0200

    Implement batch data upload and separate db data from mockup

--- src/data/firebase/firestore.ts
+++ src/data/firebase/firestore.ts
@@ -1,7 +1,9 @@
 import { User } from "firebase/auth";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, writeBatch } from "firebase/firestore";
 
 import { db } from "@/data/firebase/config";
+import ProductsData from "@/data/products-upload.json";
+import { Product } from "@/lib/definitions";
 
 export async function addUser(user: User, displayName?: string) {
   const docRef = doc(db, "users", user.uid);
@@ -15,6 +17,21 @@ export async function addUser(user: User, displayName?: string) {
         createdAt: new Date(),
       });
     }
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+export async function addProducts() {
+  const batch = writeBatch(db);
+
+  ProductsData.forEach((product: Product) => {
+    const docRef = doc(db, "products", product.slug);
+    batch.set(docRef, product);
+  });
+
+  try {
+    await batch.commit();
   } catch (error) {
     console.log("error", error);
   }
